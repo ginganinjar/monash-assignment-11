346c13e69e72aecc329b092ed3ba3973a9e320e2
David Sparrius <david@mstone.com.au>
2020-07-19 22:02:17 +1000

    api foundation work

--- server.js
+++ server.js
@@ -29,27 +29,16 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-// Displays all characters
+
+// API Routes
+// =============================================================
+//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
 app.get("/api/characters", function(req, res) {
  // return res.json(characters);
 })
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
+// * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/characters", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
@@ -65,6 +54,27 @@ app.post("/api/characters", function(req, res) {
 
   res.json(newCharacter);
 });
+
+//  * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
+// This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete
+//  a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` 
+// property, and then rewrite the notes to the `db.json` file.
+
+app.get("/api/characters/:character", function(req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+  for (var i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+
 
 
 // Starts the server to begin listening
